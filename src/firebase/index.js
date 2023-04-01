c30ebe96805b0a78af31c90da3616fdefacc6e2e
Slula <36647367+Slula@users.noreply.github.com>
2023-03-31 20:13:33 -0400

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBqNOJs8Mzw7JBcKSfhFL003kiAZwyiz74",
-    authDomain: "untitledcantogame1.firebaseapp.com",
-    projectId: "untitledcantogame1",
-    storageBucket: "untitledcantogame1.appspot.com",
-    messagingSenderId: "681922588840",
-    appId: "1:681922588840:web:0a0f22b374859bedda529d"
+    //firebase creds
   };
 
 export const app = initializeApp(firebaseConfig);
